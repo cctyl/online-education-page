ebadb15a1077a1405a88dbad8bc972f8c041ca84
cctyl <2621460357@qq.com>
2020-07-01 11:14:22 +0800

    初次提交，已完成部分功能

---
+++ guli-admin/config/dev.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  //接口地址的修改
+  BASE_API: '"http://localhost:8001"',
+})
