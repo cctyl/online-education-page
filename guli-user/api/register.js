6005d175990dcf1c6ae37d79a455730fc5067b95
cctyl <2621460357@qq.com>
2020-08-18 11:02:12 +0800

    完成用户注册

--- guli-user/api/register.js
+++ guli-user/api/register.js
@@ -1,19 +1,29 @@
 import request from '@/utils/request'
 
 export default {
-  //根据手机号码发送短信
-  getMobile(mobile) {
-    return request({
-      url: `/edumsm/send/${mobile}`,
-      method: 'get'
-    })
-  },
+
   //用户注册
-  submitRegister(formItem) {
+  userRegister(user) {
     return request({
-      url: `/ucenterservice/apimember/register`,
+
+      url: `/educenter/member/register`,
       method: 'post',
-      data: formItem
+      data: user
     })
-  }
+
+  },
+
+
+  //获取验证码
+  getCode(phone) {
+    return request({
+
+      url: `/edumsm/msm/send/${phone}`,
+      method: 'get',
+
+    })
+
+  },
+
+
 }
