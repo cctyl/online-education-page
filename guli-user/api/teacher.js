30eb35d1ca26afe5ae7df8680ca5c30ee1a7cfde
cctyl <2621460357@qq.com>
2020-08-24 11:50:18 +0800

    完成讲师数据的展示

---
+++ guli-user/api/teacher.js
@@ -0,0 +1,15 @@
+import request from '@/utils/request'
+
+export default {
+
+  //获取讲师数据
+  getPageList(page, limit) {
+    return request({
+      url: `/eduservice/teacher/front/${page}/${limit}`,
+      method: 'post'
+    })
+  }
+
+
+
+}
