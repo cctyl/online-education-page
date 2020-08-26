36a22dcdcc9631fe8ea558ec83e651332047162f
cctyl <2621460357@qq.com>
2020-08-26 09:39:56 +0800

    完成讲师详情页面的展示

--- guli-user/api/teacher.js
+++ guli-user/api/teacher.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export default {
 
-  //获取讲师数据
+  //获取讲师列表数据
   getPageList(page, limit) {
     return request({
       url: `/eduservice/teacher/front/${page}/${limit}`,
@@ -11,6 +11,12 @@ export default {
   },
 
 
-
+  //获取讲师详情以及所讲课程
+  getTeacherInfo(teacherId) {
+    return request({
+      url: `/eduservice/teacher/front/info/${teacherId}`,
+      method: 'get'
+    })
+  },
 
 }
