4c386ccf307d2100ac0e74418f741481e9d5f9ce
cctyl <2621460357@qq.com>
2020-08-28 10:20:00 +0800

    优化课程分类效果展示

--- guli-user/pages/course/index.vue
+++ guli-user/pages/course/index.vue
@@ -16,7 +16,7 @@
             <dd class="c-s-dl-li">
               <ul class="clearfix">
                 <li>
-                  <a title="全部" href="#">全部</a>
+                  <a title="全部" href="#" @click="searchAllCourse()"  :class="{active:oneIndex==9959}">全部</a>
                 </li>
                 <li v-for="(item,index) in subjectNestedList" :key="index" :class="{active:oneIndex==index}">
                   <a :title="item.title" href="#" @click="searchOne(item.id,index)"  v-text="item.title"  ></a>
@@ -221,8 +221,14 @@
         })
       },
 
+      //清空查询条件
+      searchAllCourse(){
 
-
+          this.searchObj ={};
+          this.oneIndex= 9959;
+          this.subSubjectList=[];
+        this.gotoPage(this.page)
+      },
 
       //购买量查询
       searchBuyCount(flag) {
