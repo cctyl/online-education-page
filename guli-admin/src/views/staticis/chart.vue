925e8270411b8d715e5cb2f654c64b9e86aff76e
cctyl <2621460357@qq.com>
2020-10-01 09:26:52 +0800

    修改页面布局

--- guli-admin/src/views/staticis/chart.vue
+++ guli-admin/src/views/staticis/chart.vue
@@ -3,62 +3,85 @@
     <!--表单-->
     <el-form :inline="true" class="demo-form-inline">
 
-      <el-form-item>
-        <el-select v-model="searchObj.type" clearable placeholder="请选择">
-          <el-option label="学员登录数统计" value="login_num"/>
-          <el-option label="学员注册数统计" value="register_num"/>
-          <el-option label="课程播放数统计" value="video_view_num"/>
-          <el-option label="每日课程数统计" value="course_num"/>
-        </el-select>
-      </el-form-item>
+
 
       <el-form-item>
         <el-date-picker
           v-model="searchObj.begin"
           type="date"
           placeholder="选择开始日期"
-          value-format="yyyy-MM-dd" />
+          value-format="yyyy-MM-dd"/>
       </el-form-item>
       <el-form-item>
         <el-date-picker
           v-model="searchObj.end"
           type="date"
           placeholder="选择截止日期"
-          value-format="yyyy-MM-dd" />
+          value-format="yyyy-MM-dd"/>
       </el-form-item>
       <el-button
         :disabled="btnDisabled"
         type="primary"
         icon="el-icon-search"
-        @click="showChart()">查询</el-button>
+        @click="showChart()">查询
+      </el-button>
     </el-form>
 
     <div class="chart-container">
-      <div id="chart" class="chart" style="height:500px;width:100%" />
+      <div id="chart" class="chart" style="height:250px;width:100%"/>
+      <div id="chart2" class="chart" style="height:250px;width:100%"/>
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "chart",
-      data(){
+  import echarts from 'echarts'
 
-          return({
-            searchObj:{},
-            btnDisabled:''
-          })
-      },
-      created() {
-      },
-      methods:{
-        showChart(){
+  export default {
+    name: "chart",
+    data() {
 
+      return ({
+        searchObj: {},
+        btnDisabled: false
+      })
+    },
+    created() {
+    },
+    methods: {
+      showChart() {
+        // 基于准备好的dom，初始化echarts实例
+        this.chart = echarts.init(document.getElementById('chart'))
+        this.chart2 = echarts.init(document.getElementById('chart2'))
+
+
+        // 指定图表的配置项和数据
+        var option = {
+          // x轴是类目轴（离散数据）,必须通过data设置类目数据
+          xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+          },
+          // y轴是数据轴（连续数据）
+          yAxis: {
+            type: 'value'
+          },
+          // 系列列表。每个系列通过 type 决定自己的图表类型
+          series: [{
+            // 系列中的数据内容数组
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            // 折线图
+            type: 'line'
+          }]
         }
+
+        this.chart.setOption(option)
+        this.chart2.setOption(option)
       }
-
-
     }
+
+
+  }
 </script>
 
 <style scoped>
