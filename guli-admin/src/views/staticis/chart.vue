f6dee9eafb3056e52d166197af5ed873d34d8f6e
cctyl <2621460357@qq.com>
2020-10-02 15:40:24 +0800

    完成统计图表展示

--- guli-admin/src/views/staticis/chart.vue
+++ guli-admin/src/views/staticis/chart.vue
@@ -2,9 +2,6 @@
   <div class="app-container">
     <!--表单-->
     <el-form :inline="true" class="demo-form-inline">
-
-
-
       <el-form-item>
         <el-date-picker
           v-model="searchObj.begin"
@@ -28,39 +25,69 @@
     </el-form>
 
     <div class="chart-container">
-      <div id="chart" class="chart" style="height:250px;width:100%"/>
-      <div id="chart2" class="chart" style="height:250px;width:100%"/>
+      <div id="view" class="chart" style="height:250px;width:100%"/>
+      <div id="login" class="chart" style="height:250px;width:100%"/>
+      <div id="register" class="chart" style="height:250px;width:100%"/>
+      <div id="course" class="chart" style="height:250px;width:100%"/>
     </div>
   </div>
 </template>
 
 <script>
   import echarts from 'echarts'
+  import staticApi from '@/api/sta/staticis.js'
 
   export default {
     name: "chart",
     data() {
 
       return ({
-        searchObj: {},
-        btnDisabled: false
+        searchObj: {
+          begin:'',
+          end:''
+        },
+        btnDisabled: false,
+        viewList:[],
+        loginList:[],
+        registerList:[],
+        courseList:[],
+        dateList:[],
+
       })
     },
-    created() {
-    },
+
     methods: {
-      showChart() {
+      showChart(){
+        staticApi.getDailyCount(this.searchObj).then(result => {
+          console.log(result);
+          this.viewList = result.data.viewList;
+          this.loginList = result.data.loginList;
+          this.registerList = result.data.registerList;
+          this.courseList = result.data.courseList;
+          this.dateList = result.data.dateList;
+
+          this.setChart();
+        }).catch(reason => {
+
+        });
+
+      },
+
+      setChart() {
         // 基于准备好的dom，初始化echarts实例
-        this.chart = echarts.init(document.getElementById('chart'))
-        this.chart2 = echarts.init(document.getElementById('chart2'))
-
-
+        this.viewChart = echarts.init(document.getElementById('view'));
+        this.loginChart = echarts.init(document.getElementById('login'))
+        this.registerChart = echarts.init(document.getElementById('register'))
+        this.courseChart = echarts.init(document.getElementById('course'))
         // 指定图表的配置项和数据
-        var option = {
+        var viewOption = {
+          title: {
+            text: '日播放数'
+          },
           // x轴是类目轴（离散数据）,必须通过data设置类目数据
           xAxis: {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            data: this.dateList
           },
           // y轴是数据轴（连续数据）
           yAxis: {
@@ -69,14 +96,79 @@
           // 系列列表。每个系列通过 type 决定自己的图表类型
           series: [{
             // 系列中的数据内容数组
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            data: this.viewList,
             // 折线图
             type: 'line'
           }]
-        }
+        };
+        var loginOption = {
+          title: {
+            text: '日登陆数'
+          },
+          // x轴是类目轴（离散数据）,必须通过data设置类目数据
+          xAxis: {
+            type: 'category',
+            data: this.dateList
+          },
+          // y轴是数据轴（连续数据）
+          yAxis: {
+            type: 'value'
+          },
+          // 系列列表。每个系列通过 type 决定自己的图表类型
+          series: [{
+            // 系列中的数据内容数组
+            data: this.loginList,
+            // 折线图
+            type: 'line'
+          }]
+        };
+        var registerOption = {
+          title: {
+            text: '日注册人数'
+          },
+          // x轴是类目轴（离散数据）,必须通过data设置类目数据
+          xAxis: {
+            type: 'category',
+            data: this.dateList
+          },
+          // y轴是数据轴（连续数据）
+          yAxis: {
+            type: 'value'
+          },
+          // 系列列表。每个系列通过 type 决定自己的图表类型
+          series: [{
+            // 系列中的数据内容数组
+            data: this.registerList,
+            // 折线图
+            type: 'line'
+          }]
+        };
+        var courseOption = {
+          title: {
+            text: '日新增课程数'
+          },
+          // x轴是类目轴（离散数据）,必须通过data设置类目数据
+          xAxis: {
+            type: 'category',
+            data: this.dateList
+          },
+          // y轴是数据轴（连续数据）
+          yAxis: {
+            type: 'value'
+          },
+          // 系列列表。每个系列通过 type 决定自己的图表类型
+          series: [{
+            // 系列中的数据内容数组
+            data: this.courseList,
+            // 折线图
+            type: 'line'
+          }]
+        };
 
-        this.chart.setOption(option)
-        this.chart2.setOption(option)
+        this.viewChart.setOption(viewOption);
+        this.loginChart.setOption(loginOption);
+        this.registerChart.setOption(registerOption);
+        this.courseChart.setOption(courseOption);
       }
     }
 
