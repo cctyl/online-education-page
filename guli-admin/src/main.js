9345cabce1ecbb4b42262a07d643e44ab10c9e3a
cctyl <2621460357@qq.com>
2020-10-22 16:39:36 +0800

    修改store，添加权限

--- guli-admin/src/main.js
+++ guli-admin/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
-import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
 
 import '@/styles/index.scss' // global css
 
@@ -13,10 +14,12 @@ import store from './store'
 
 import '@/icons' // icon
 import '@/permission' // permission control
+import { hasBtnPermission } from './utils/permission' // button permission
 
 Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
+Vue.prototype.hasPerm = hasBtnPermission
 
 new Vue({
   el: '#app',
